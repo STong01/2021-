f802015faa15b29adf7c52e2d73cb25e630667f3
STong01 <1461638919@qq.com>
2020-09-18 21:22:21 +0800

    快速排序

--- JZ.22-2/JZ.22-2/链表中倒数第k个节点.cpp
+++ JZ.22-2/JZ.22-2/链表中倒数第k个节点.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 using namespace std;
 
+int partion(int* arr, int m, int n)
+{
+	int key = arr[m];
+	while (m < n)
+	{
+		while (m < n && arr[n] > key)
+		{
+			--n;
+		}
+		arr[m] = arr[n];
+		while (m < n && arr[m] < key)
+		{
+			++m;
+		}
+		arr[n] = arr[m];
+	}
+	arr[m] = key;
+	return m;
+}
+
+void QSORT(int* arr, int m, int n)
+{
+	if (m < n)
+	{
+		int mid = partion(arr, m, n);
+		QSORT(arr, m, mid - 1);
+		QSORT(arr, mid + 1, n);
+	}
+}
+
+void qsort1(int* arr, int len)
+{
+	QSORT(arr, 0, len - 1);
+}
+
+
+void test()
+{
+	int arr[] = { 34, 5, 623, 4, 614, 41, 56, 778 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	qsort1(arr, len);
+	for (int i = 0; i < len; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl; 
+}
+
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
+
 struct ListNode
 {
 	int m_nValue;
@@ -68,9 +123,3 @@ ListNode* FindKthToTail2(ListNode* pListHead, unsigned int k)
 	return pBehead;
 }
 
-int main()
-{
-
-	system("pause");
-	return 0;
-}
